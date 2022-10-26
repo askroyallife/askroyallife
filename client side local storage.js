e970720babcb8e886786ed942a5b1204df8abea7
Abhinav Singh <92722725+askroyallife@users.noreply.github.com>
2022-10-26 13:26:42 +0530

    pls choose the required js file

---
+++ client side local storage.js
@@ -0,0 +1,24 @@
+localStorage : {
+    user :{ token:xxxxxxxxxxx, name:xxxxxxxxxxx,email :xxxxxxxxxxx},
+    documents :[
+0==> {documentobject1},
+1==> "",
+2==>{documentobject3}]
+}
+
+
+
+
+documentObject : {
+    server_key : -1,
+    local_key :0,
+    document :"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+saveUsertoLocal (userObject)
+deleteUserInLocal (userObject)
+
+newDocumentinLocal(documentObject) return documentObject
+updateDocumentINLocal (documentObject)
+deleteDocumentInLocal(documentObject/1) 
+
+
